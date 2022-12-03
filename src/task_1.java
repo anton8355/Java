2a4512de97762675130b8bb3c1d37d22e1c3cc9b
Anton <anton.fleer03@mail.ru>
2022-12-03 13:33:48 +0300

    1234

--- src/task_1.java
+++ src/task_1.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main {
+public class task_1 {
     public static int Factorial(int num) //Метод для нахождения факториала числа
     {
         String str = String.valueOf(num);
